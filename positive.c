50d6244bb83a358087131d127887a7e739d539f8
santhiyasanthra <31505577+santhiyasanthra@users.noreply.github.com>
2018-01-08 23:14:51 +0530

    evenodd.c

---
+++ positive.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int num;
+scanf("enter the num=%d",num);
+if(num>0)
+{
+printf("the given number is positive");
+}
+if(num=0)
+{
+printf("the given number is zero");
+}if(num<0)
+{
+printf("the given number is negative");
+}
+return 0;
+}
+getch();
