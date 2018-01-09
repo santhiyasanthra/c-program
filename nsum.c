c0bef35a6233f1b100a5d0df5790a412814de9c8
santhiyasanthra <31505577+santhiyasanthra@users.noreply.github.com>
2018-01-09 22:36:39 +0530

    Create nsum.c

---
+++ nsum.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+int i,n,nsum=0;
+scanf("%d",&n);
+for(i=1;i<n;i++)
+{
+	nsum=nsum+i;
+
+}
+	printf("\n the sum of n natural number is %d",nsum);
+return 0;
+	
+}
+getch();
